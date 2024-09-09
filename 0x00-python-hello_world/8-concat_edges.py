21710a9ff7b84094041d891b97263456fa7fe5fe
Moussa Sabbar <sabbarmoussa@gmail.com>
2024-09-09 21:41:58 +0100

    object-oriented with Python

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+str = str[36:62] + " " + str[-19:-15] + " " +str[:6]
+print(str)
