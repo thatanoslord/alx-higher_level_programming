e802eeca54133a8541ab78626114eb883ea6c81e
Moussa Sabbar <sabbarmoussa@gmail.com>
2024-09-09 21:53:08 +0100

    new thing

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-language that combines remarkable power with very clear syntax"
-str = str[36:62] + " " + str[-19:-15] + " " +str[:6]
+        language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:110] + str[:6]
 print(str)
+
