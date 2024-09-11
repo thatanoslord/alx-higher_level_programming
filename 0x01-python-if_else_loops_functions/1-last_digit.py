c342a1150715460432f5887a2422cc68ff7551fb
Moussa Sabbar <sabbarmoussa@gmail.com>
2024-09-11 01:00:59 +0100

    fixed the issue

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ number_str = str(number)
 last_number = int(number_str[-1])
 if last_number > 5:
     str = "and is greater than 5"
-elif last_number = 0:
+elif last_number == 0:
     str  = "and is 0"
 else:
     str = "and is less than 6 and not 0"
