4ba7c970fed3449512801e0f5327db00be290042
Moussa Sabbar <sabbarmoussa@gmail.com>
2024-09-21 00:55:00 +0100

    new modif

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
+def sub_roman(a, b):
+    return a - (b*2)
+
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+    if not roman_string or not isinstance(roman_string, str):
         return 0
 
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    prev_dig = 0
     result = 0
-    length = len(roman_string)
 
-    for i in range(length):
-        if i < length - 1 and roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]:
-            result -= roman_dict[roman_string[i]]
-        else:
-            result += roman_dict[roman_string[[i]]
-    
+     for r in roman_string:
+        if r in romans:
+            if romans[r] > prev_dig:
+                result += sub_roman(romans[r], prev_dig)
+            else:
+                result += romans[r]
+            prev_dig = romans[r]
+
     return result
-
