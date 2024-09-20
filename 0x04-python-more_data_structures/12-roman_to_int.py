bdbd8e9eda683e41fc2495a526b81e418f4a985e
Moussa Sabbar <sabbarmoussa@gmail.com>
2024-09-21 00:48:49 +0100

    roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    length = len(roman_string)
+
+    for i in range(length):
+        if i < length - 1 and roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]:
+            result -= roman_dict[roman_string[i]]
+        else:
+            result += roman_dict[roman_string[[i]]
+    
+    return result
+
