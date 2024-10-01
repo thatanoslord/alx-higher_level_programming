495fc23a8ec22ae5ebe17113f8038bd91aa218e0
Moussa Sabbar <sabbarmoussa@gmail.com>
2024-10-02 00:57:54 +0100

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Defining A square class """
+
+
+class Square:
+    """ Square """
+
+    def __init__(self, size):
+        """initialize square
+
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
