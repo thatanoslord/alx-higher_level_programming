2eb768e3c28f8c751fe3ee472d5af774b8189050
MOUSSA SABBAR <62625289+thatanoslord@users.noreply.github.com>
2024-10-14 23:18:51 +0100

    Create 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""is_same_class: checks if the object is an instance of a class"""
+
+
+def is_same_class(obj, a_class):
+    """is_same_class"""
+    return type(obj) is a_class
