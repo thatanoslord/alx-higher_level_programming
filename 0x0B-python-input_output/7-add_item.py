254e1c736a3692ee6a04ac8eff1841a39b08e223
MOUSSA SABBAR <62625289+thatanoslord@users.noreply.github.com>
2024-10-16 22:10:41 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
+""" add args to a python list and write them to a file"""
 import sys
-import os
 
-# Import the required functions from previous tasks
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__(
+        '6-load_from_json_file').load_from_json_file
 
-# Define the filename for the JSON file
-filename = "add_item.json"
-
-# Try to load the existing data from the file, if it exists
-if os.path.exists(filename):
-    items = load_from_json_file(filename)
-else:
-    items = []
-
-# Add all the command-line arguments to the list
-items.extend(sys.argv[1:])
-
-# Save the updated list to the file
-save_to_json_file(items, filename)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
