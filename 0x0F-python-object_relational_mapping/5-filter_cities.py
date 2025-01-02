619a156298ca1aca894393a092325e1b9ca9394f
MOUSSA SABBAR <62625289+thatanoslord@users.noreply.github.com>
2025-01-02 01:08:40 +0100

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""  lists all of states from the database of hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
@@ -7,12 +7,12 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""SELECT cities.name FROM
+    cu = db.cusor()
+    cu.execute("""SELECT cities.name FROM
                 cities INNER JOIN states ON states.id=cities.state_id
                 WHERE states.name=%s""", (sys.argv[4],))
-    rows = cur.fetchall()
+    rows = cu.fetchall()
     tmp = list(row[0] for row in rows)
     print(*tmp, sep=", ")
-    cur.close()
+    cu.close()
     db.close()
