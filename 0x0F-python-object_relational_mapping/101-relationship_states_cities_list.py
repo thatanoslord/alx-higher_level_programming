1cb83d1b55be484c6627e4d86a30beb2700b7160
MOUSSA SABBAR <62625289+thatanoslord@users.noreply.github.com>
2025-01-02 01:28:11 +0100

    Update 101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -10,24 +10,21 @@ from relationship_state import State
 from relationship_city import City
 
 if __name__ == "__main__":
-    # Parse command-line arguments
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # Create the database engine
+
     engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
 
-    # Create a configured "Session" class
+
     Session = sessionmaker(bind=engine)
 
-    # Create a session
     session = Session()
 
-    # Query states and their cities
     states = session.query(State).order_by(State.id, City.id).all()
 
-    # Print results
     for state in states:
         print(f"{state.id}: {state.name}")
         for city in state.cities:
